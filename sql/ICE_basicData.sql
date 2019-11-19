d8a4257f98a73fe8dc50b18996422c4147c875b1
LudwigLiu-CN <17623857792@163.com>
2019-11-19 15:20:40 +0800

    Basic data

---
+++ sql/ICE_basicData.sql
@@ -0,0 +1,212 @@
+SET SQL_SAFE_UPDATES = 0;
+
+insert into categories values(0, 'Action');
+insert into categories values(0, 'Adventure');
+insert into categories values(0, 'Arcade');
+insert into categories values(0, 'Board Games');
+insert into categories values(0, 'Racing');
+insert into categories values(0, 'Role-Playing Games');
+insert into categories values(0, 'Shooter');
+insert into categories values(0, 'Simulation');
+insert into categories values(0, 'Sports');
+insert into categories values(0, 'Strategy');
+
+
+insert into consoles values(0, 'PS3');
+insert into consoles values(0, 'PS4');
+insert into consoles values(0, 'PS Vita');
+insert into consoles values(0, 'PSP');
+insert into consoles values(0, 'Nintendo Switch');
+insert into consoles values(0, 'Nintendo 3DS');
+insert into consoles values(0, 'xbox 360');
+insert into consoles values(0, 'xbox one');
+
+
+insert into publishers(PUBLISHER_ID, PUBLISHER_NAME, PWD) values(0, 'Kojima Productions', 123456);
+insert into publishers(PUBLISHER_ID, PUBLISHER_NAME, PWD) values(0, 'Naughty Dog', 123456);
+insert into publishers(PUBLISHER_ID, PUBLISHER_NAME, PWD) values(0, 'CD PROJEkT RED', 123456);
+insert into publishers(PUBLISHER_ID, PUBLISHER_NAME, PWD) values(0, 'FromSoftware', 123456);
+insert into publishers(PUBLISHER_ID, PUBLISHER_NAME, PWD) values(0, 'Rockstar', 123456);
+insert into publishers(PUBLISHER_ID, PUBLISHER_NAME, PWD) values(0, 'Naughty Dog', 123456);
+insert into publishers(PUBLISHER_ID, PUBLISHER_NAME, PWD) values(0, 'Santa Monica Studio', 123456);
+insert into publishers(PUBLISHER_ID, PUBLISHER_NAME, PWD) values(0, 'CAPCOM', 123456);
+insert into publishers(PUBLISHER_ID, PUBLISHER_NAME, PWD) values(0, 'Nintendo', 123456);
+insert into publishers(PUBLISHER_ID, PUBLISHER_NAME, PWD) values(0, 'KONAMI', 123456);
+insert into publishers(PUBLISHER_ID, PUBLISHER_NAME, PWD) values(0, 'SQUARE ENIX', 123456);
+insert into publishers(PUBLISHER_ID, PUBLISHER_NAME, PWD) values(0, 'Ubisoft', 123456);
+insert into publishers(PUBLISHER_ID, PUBLISHER_NAME, PWD) values(0, 'OBSIDIAN STUDIO', 123456);
+insert into publishers(PUBLISHER_ID, PUBLISHER_NAME, PWD) values(0, 'Koei Tecmo', 123456);
+insert into publishers(PUBLISHER_ID, PUBLISHER_NAME, PWD) values(0, 'Turn10', 123456);
+insert into publishers(PUBLISHER_ID, PUBLISHER_NAME, PWD) values(0, 'Polyphony Digital Inc.', 123456);
+insert into publishers(PUBLISHER_ID, PUBLISHER_NAME, PWD) values(0, 'Guerrilla', 123456);
+insert into publishers(PUBLISHER_ID, PUBLISHER_NAME, PWD) values(0, 'Blizzard Entertainment', 123456);
+insert into publishers(PUBLISHER_ID, PUBLISHER_NAME, PWD) values(0, 'Team Cherry', 123456);
+insert into publishers(PUBLISHER_ID, PUBLISHER_NAME, PWD) values(0, 'Bethesda Softworks', 123456);
+insert into publishers(PUBLISHER_ID, PUBLISHER_NAME, PWD) values(0, 'Respawn', 123456);
+insert into publishers(PUBLISHER_ID, PUBLISHER_NAME, PWD) values(0, '343 Industries', 123456);
+insert into publishers(PUBLISHER_ID, PUBLISHER_NAME, PWD) values(0, 'Remedy', 123456);
+insert into publishers(PUBLISHER_ID, PUBLISHER_NAME, PWD) values(0, 'Activation', 123456);
+insert into publishers(PUBLISHER_ID, PUBLISHER_NAME, PWD) values(0, '2K Games', 123456);
+insert into publishers(PUBLISHER_ID, PUBLISHER_NAME, PWD) values(0, 'EA', 123456);
+insert into publishers(PUBLISHER_ID, PUBLISHER_NAME, PWD) values(0, 'Activision', 123456);
+
+
+insert into tags values(0, 'Indie');
+insert into tags values(0, 'Avtion');
+insert into tags values(0, 'Adventure');
+insert into tags values(0, 'Casual');
+insert into tags values(0, 'Strategy');
+insert into tags values(0, 'Simulation');
+insert into tags values(0, 'RPG');
+insert into tags values(0, 'Singleplayer');
+insert into tags values(0, 'voilence');
+insert into tags values(0, 'Sports');
+insert into tags values(0, 'Multiplayer');
+insert into tags values(0, 'Racing');
+insert into tags values(0, 'Great Soundtrack');
+insert into tags values(0, '2D');
+insert into tags values(0, '3D');
+insert into tags values(0, '2.5D');
+insert into tags values(0, 'Puzzle');
+insert into tags values(0, 'Story Rich');
+insert into tags values(0, 'Difficult');
+insert into tags values(0, 'Fantacy');
+insert into tags values(0, 'VR');
+insert into tags values(0, 'Horror');
+insert into tags values(0, 'Funny');
+insert into tags values(0, 'Sci-fi');
+insert into tags values(0, 'Shooter');
+insert into tags values(0, 'Open World');
+insert into tags values(0, 'Pixel Graphics');
+insert into tags values(0, 'First-Person');
+insert into tags values(0, 'Platformer');
+insert into tags values(0, 'Survival');
+insert into tags values(0, 'Sandbox');
+insert into tags values(0, 'Exploration');
+insert into tags values(0, 'Co-Op');
+insert into tags values(0, 'Movie');
+insert into tags values(0, 'Replay Value');
+insert into tags values(0, 'Third Person');
+insert into tags values(0, 'Space');
+insert into tags values(0, 'Zombies');
+insert into tags values(0, 'Dark');
+insert into tags values(0, 'Mystery');
+insert into tags values(0, 'Realistic');
+insert into tags values(0, 'War');
+insert into tags values(0, 'Music');
+insert into tags values(0, 'Walking Simulator');
+insert into tags values(0, 'PvP');
+insert into tags values(0, 'PvE');
+insert into tags values(0, 'Fighting');
+insert into tags values(0, 'Magic');
+insert into tags values(0, 'Military');
+insert into tags values(0, 'Cyberpunk');
+insert into tags values(0, 'Blood');
+insert into tags values(0, 'Driving');
+insert into tags values(0, 'Epic');
+insert into tags values(0, 'Cult');
+insert into tags values(0, 'Logic');
+insert into tags values(0, 'Demons');
+insert into tags values(0, 'Remake');
+insert into tags values(0, 'Remaster');
+insert into tags values(0, 'Ninja');
+insert into tags values(0, 'Runner');
+insert into tags values(0, 'Cold War');
+insert into tags values(0, 'Stealth');
+
+
+insert into users(user_id, user_name, gender, pwd) values(0, 'LudwigLiu-CN', true, 123456);
+insert into users(user_id, user_name, gender, pwd) values(0, 'threethousands', true, 123456);
+insert into users(user_id, user_name, gender, pwd) values(0, 'LXF-DX3906', true, 123456);
+insert into users(user_id, user_name, gender, pwd) values(0, 'herobrine1010', true, 123456);
+
+
+/*GAME 1*/
+insert into games(GAME_ID, TITLE, PRICE, DISCOUNT, RELEASE_DATE, PRE_ORDER, RATE_COUNT, ON_SALE) values(0, 'DEATH STRANDING', 59.99, false, '2019-11-08', false, 0, true);
+insert into sale_game values(1, 1);
+insert into played_on values(1, 2);
+insert into belong values(1, 2);
+insert into has_tag values(1, 3);
+insert into has_tag values(1, 7);
+insert into has_tag values(1, 18);
+insert into has_tag values(1, 34);
+insert into has_tag values(1, 53);
+
+/*GAME 2*/
+insert into games(GAME_ID, TITLE, PRICE, DISCOUNT, RELEASE_DATE, PRE_ORDER, RATE_COUNT, ON_SALE) values(0, 'The Last Of Us', 9.99, false, '2013-06-14', false, 0, true);
+insert into sale_game values(2, 2);
+insert into played_on values(2, 1);
+insert into belong values(2, 2);
+insert into has_tag values(2, 2);
+insert into has_tag values(2, 3);
+insert into has_tag values(2, 9);
+insert into has_tag values(2, 22);
+insert into has_tag values(2, 24);
+insert into has_tag values(2, 38);
+insert into has_tag values(2, 47);
+
+/*GAME 3*/
+insert into games(GAME_ID, TITLE, PRICE, DISCOUNT, RELEASE_DATE, PRE_ORDER, RATE_COUNT, ON_SALE) values(0, 'The Last Of Us Remastered', 19.99, false, '2014-07-29', false, 0, true);
+insert into sale_game values(2, 3);
+insert into played_on values(2, 2);
+insert into belong values(3, 2);
+insert into has_tag values(3, 2);
+insert into has_tag values(3, 3);
+insert into has_tag values(3, 9);
+insert into has_tag values(3, 22);
+insert into has_tag values(3, 38);
+insert into has_tag values(3, 47);
+insert into has_tag values(3, 58);
+
+/*GAME 4*/
+insert into games(GAME_ID, TITLE, PRICE, DISCOUNT, RELEASE_DATE, PRE_ORDER, RATE_COUNT, ON_SALE) values(0, 'UNCHARTED 4: A Thief\'s End', 19.99, false, '2016-05-10', false, 0, true);
+insert into sale_game values(2, 4);
+insert into played_on values(4, 2);
+insert into belong values(4, 2);
+insert into has_tag values(4, 2);
+insert into has_tag values(4, 3);
+insert into has_tag values(4, 17);
+insert into has_tag values(4, 60);
+
+/*GAME 5*/
+insert into games(GAME_ID, TITLE, PRICE, DISCOUNT, RELEASE_DATE, PRE_ORDER, RATE_COUNT, ON_SALE) values(0, 'The Last Of Us Part II', 59.99, false, '2020-05-29', true, 0, true);
+insert into sale_game values(2, 5);
+insert into played_on values(5, 2);
+insert into belong values(5, 2);
+insert into has_tag values(5, 2);
+insert into has_tag values(5, 3);
+insert into has_tag values(5, 9);
+insert into has_tag values(5, 22);
+insert into has_tag values(5, 38);
+insert into has_tag values(5, 47);
+
+/*GAME 6*/
+insert into games(GAME_ID, TITLE, PRICE, DISCOUNT, RELEASE_DATE, PRE_ORDER, RATE_COUNT, ON_SALE) values(0, 'The Witcher 3: Wild Hunt', 39.99, false, '2015-05-18', false, 0, true);
+insert into sale_game values(3, 6);
+insert into played_on values(6, 2);
+insert into played_on values(6, 5);
+insert into played_on values(6, 8);
+insert into belong values(6, 6);
+insert into has_tag values(6, 2);
+insert into has_tag values(6, 3);
+insert into has_tag values(6, 7);
+insert into has_tag values(6, 8);
+insert into has_tag values(6, 20);
+insert into has_tag values(6, 26);
+insert into has_tag values(6, 35);
+insert into has_tag values(6, 47);
+insert into has_tag values(6, 48);
+
+/*GAME 7*/
+insert into games(GAME_ID, TITLE, PRICE, DISCOUNT, RELEASE_DATE, PRE_ORDER, RATE_COUNT, ON_SALE) values(0, 'Cyberpunk 2077', 59.99, false, '2020-04-16', true, 0, true);
+insert into sale_game values(3, 7);
+insert into played_on values(7, 2);
+insert into played_on values(7, 8);
+insert into belong values(7, 6);
+insert into has_tag values(7, 2);
+insert into has_tag values(7, 3);
+insert into has_tag values(7, 9);
+insert into has_tag values(7, 24);
+insert into has_tag values(7, 26);
+insert into has_tag values(7, 28);
+insert into has_tag values(7, 50);
